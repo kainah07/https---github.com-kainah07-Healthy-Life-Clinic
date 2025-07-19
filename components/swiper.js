5d04b4cb41a8806b23f16e00a26be2b2afd0117b
kai <shekinahjoycanosa7@gmail.com>
2025-07-19 09:25:02 +0800

    made the navbar sticky

--- components/swiper.js
+++ components/swiper.js
@@ -34,3 +34,15 @@ button.addEventListener('mouseenter', () => {
 button.addEventListener('mouseleave', () => {
   icon.src = 'images/login-black.svg';
 });
+
+
+
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('mainNavbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('navbar-scrolled');
+  } else {
+    navbar.classList.remove('navbar-scrolled');
+  }
+});
+
